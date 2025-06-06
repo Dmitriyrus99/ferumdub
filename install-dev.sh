c363d70736177c46e23019ef372dda38412bfe39
Dmitriyrus99 <Dmitriyrus99@gmail.com>
2025-06-06 21:13:41 +0300

    Add development setup scripts and fixtures

---
+++ install-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# create Python virtual environment if not exists
+if [ ! -d .venv ]; then
+    python3 -m venv .venv
+fi
+
+source .venv/bin/activate
+pip install -U pip
+pip install -r requirements.txt
